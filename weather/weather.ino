9fc2404cd6ea43794702156f3c6e422aec554c5b
alekso56 <simple_and_easy@hotmail.com>
2015-06-30 10:37:29 +0200

    rework the thing

--- weather/weather.ino
+++ weather/weather.ino
@@ -21,134 +21,113 @@ RF24 radio(9, 10);
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };   // Radio pipe addresses for the 2 nodes to communicate.
 typedef enum { wdt_16ms = 0, wdt_32ms, wdt_64ms, wdt_128ms, wdt_250ms, wdt_500ms, wdt_1s, wdt_2s, wdt_4s, wdt_8s } wdt_prescalar_e;
 bool isWeatherStation = true;
+float weatherData[6];//temp,pressure,altitude in meters,Lux intensity,current,,solarvolt,
 
 void setup() {
-    radio.begin();
+  radio.begin();
+  if (isWeatherStation) {
+    radio.powerDown();
+    pinMode(5, OUTPUT);//bmp
+    pinMode(4, OUTPUT);//current sensor
+    pinMode(3, OUTPUT);//solar voltage sensor
+    pinMode(2, OUTPUT);//light sensor
+    pinMode(A1, INPUT);//current sensor
+    pinMode(A0, INPUT);//solar voltage sensor
+  } else {
+    radio.startListening();
+  }
 }
 
-void sendWeatherData(){
-  pinMode(2, OUTPUT);
+void getLightSensorData() {
   digitalWrite(2, HIGH); //light sensor
-  pinMode(5, OUTPUT);
-  digitalWrite(5, HIGH); //bmp
-  delay(500);
-  //Serial.begin(9600);
   LightSensor.begin();
   LightSensor.SetAddress(Device_Address_L);
   LightSensor.SetMode(Continuous_H_resolution_Mode);
-  //Serial.println("lightRunning...");
+  delay(50);
+  weatherData[3] = LightSensor.GetLightIntensity();
+  digitalWrite(2, LOW); //light sensor
+}
+void getBMPData() {
+  digitalWrite(5, HIGH); //bmp
+  delay(50);
   if (!bmp.begin()) {
-    //Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-    while (1) {}
+    // Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    digitalWrite(5, LOW); //bmp
+    return;
   }
-  radio.powerUp();          
+  weatherData[0] = bmp.readTemperature();
+  weatherData[1] = bmp.readPressure(); //Pa
+  // Calculate altitude assuming 'standard' barometric
+  // pressure of 1013.25 millibar = 101325 Pascal
+  weatherData[2] = bmp.readAltitude(101400); //meters
+  digitalWrite(5, LOW); //bmp
+}
+void getCurrentData() {
+  digitalWrite(4, HIGH); //current
+  weatherData[4] = analogRead(A1); //current
+  digitalWrite(4, LOW); //current
+}
+void getVoltageData() {
+  digitalWrite(3, HIGH); //voltage
+  weatherData[5]  = analogRead(A0) * (15.8 / 1023.0);
+  digitalWrite(3, LOW); //voltage
+}
+void sendWeatherData() {
+  radio.powerUp();
   //sender
-  //radio.openWritingPipe(pipes[0]);
-  //radio.openReadingPipe(1,pipes[1]);
+  radio.openWritingPipe(pipes[0]);
+  //radio.openReadingPipe(1, pipes[1]);
 
-  //receiver
-  radio.openWritingPipe(pipes[1]);
-  radio.openReadingPipe(1, pipes[0]);
+  radio.write( &weatherData, sizeof(weatherData) );
+  // Shut down the system
+  delay(500);                     // Experiment with some delay here to see if it has an effect
+  // Power down the radio.
+  radio.powerDown();              // NOTE: The radio MUST be powered back up again manually
+}
 
+void debugData() {
+  Serial.begin(9600);
+  Serial.print(weatherData[0]);
+  Serial.println(" C");
+  Serial.print(weatherData[1]);
+  Serial.println(" Pa");
+  Serial.print(weatherData[2]);
+  Serial.println(" meters");
+  Serial.print(weatherData[3]);
+  Serial.println(" lux");
+  Serial.print(weatherData[4]);
+  Serial.println(" current");
+  Serial.print(weatherData[5]);
+  Serial.println(" volts");
+  delay(5);
+  Serial.end();
 }
 
 void loop() {
-  // Pong back role.  Receive each packet, dump it out, and send it back
   if (isWeatherStation) {
-    if ( radio.available() ) {                                  // if there is data ready, pls mov
-      Serial.print("Temperature = ");
-      Serial.print(bmp.readTemperature());
-      Serial.println(" *C");
-
-      Serial.print("Pressure = ");
-      Serial.print(bmp.readPressure());
-      Serial.println(" Pa");
-
-      // Calculate altitude assuming 'standard' barometric
-      // pressure of 1013.25 millibar = 101325 Pascal
-      Serial.print("Altitude = ");
-      Serial.print(bmp.readAltitude());
-      Serial.println(" meters");
-
-      Serial.print("Pressure at sealevel (calculated) = ");
-      Serial.print(bmp.readSealevelPressure());
-      Serial.println(" Pa");
-
-      // you can get a more precise measurement of altitude
-      // if you know the current sea level pressure which will
-      // vary with weather and such. If it is 1015 millibars
-      // that is equal to 101500 Pascals.
-      Serial.print("Real altitude = ");
-      Serial.print(bmp.readAltitude(101400));
-      Serial.println(" meters");
-
-      Serial.println();
-
-      uint16_t lux = LightSensor.GetLightIntensity();
-      Serial.print("Light: ");
-      Serial.print(lux);
-      Serial.println(" lx");
-wa
-      unsigned long got_time;
+    getLightSensorData();
+    getBMPData();
+    getCurrentData();
+    getVoltageData();
+    // sendWeatherData();
+    debugData();
+    delay(500);
+  }
+  if (not isWeatherStation)  {
+    //receiver
+    //radio.openWritingPipe(pipes[1]);
+    radio.openReadingPipe(1, pipes[0]);
+    if ( radio.available() ) {                                  // if there is data ready
       while (radio.available()) {                             // Dump the payloads until we've gotten everything
-        radio.read( &got_time, sizeof(unsigned long) );       // Get the payload, and see if this was the last one.
-        // Spew it.  Include our time, because the ping_out millis counter is unreliable
-        printf("Got payload %lu @ %lu...", got_time, millis()); // due to it sleeping
+        radio.read( &weatherData, sizeof(weatherData) );       // Get the payload, and see if this was the last one.
       }
-
-      radio.stopListening();                                  // First, stop listening so we can talk
-      radio.write( &got_time, sizeof(unsigned long) );        // Send the final one back.
-      printf("Sent response.\n\r");
-      radio.startListening();                                 // Now, resume listening so we catch the next packets.
+      debugData();
     } else {
       delay(50);                                             // Delay so the serial data can print out
       do_sleep();
 
     }
   }
-  if (not isWeatherStation)  {                     // stuff should be moved around lol, fix pls
-    radio.powerUp();                                // Power up the radio after sleeping
-    radio.stopListening();                          // First, stop listening so we can talk.
-
-    unsigned long time = millis();                  // Take the time, and send it.
-    printf("Now sending... %lu \n\r", time);
-
-    radio.write( &time, sizeof(unsigned long) );
-
-    radio.startListening();                         // Now, continue listening
-
-    unsigned long started_waiting_at = millis();    // Wait here until we get a response, or timeout (250ms)
-    bool timeout = false;
-    while ( ! radio.available()  ) {
-      if (millis() - started_waiting_at > 250 ) { // Break out of the while loop if nothing available
-        timeout = true;
-        break;
-      }
-    }
-
-    if ( timeout ) {                                // Describe the results
-      printf("Failed, response timed out.\n\r");
-    } else {
-      unsigned long got_time;                     // Grab the response, compare, and send to debugging spew
-      radio.read( &got_time, sizeof(unsigned long) );
-
-      printf("Got response %lu, round-trip delay: %lu\n\r", got_time, millis() - got_time);
-    }
-
-    // Shut down the system
-    delay(500);                     // Experiment with some delay here to see if it has an effect
-    // Power down the radio.
-    radio.powerDown();              // NOTE: The radio MUST be powered back up again manually
-
-    // Sleep the MCU.
-    do_sleep();
-
-
-  }
-}
-
-void wakeUp() {
-  sleep_disable();
 }
 
 // Sleep helpers
@@ -166,18 +145,15 @@ void setup_watchdog(uint8_t prescalar) {
   WDTCSR = _BV(WDCE) | _BV(WDE);            // Write the WD Change enable bit to enable changing the prescaler and enable system reset
   WDTCSR = _BV(WDCE) | wdtcsr | _BV(WDIE);  // Write the prescalar bits (how long to sleep, enable the interrupt to wake the MCU
 }
-
-ISR(WDT_vect)
-{
-  //--sleep_cycles_remaining;
-  Serial.println("WDT");
+void WakeUP() {
+  sleep_disable();
 }
 
 void do_sleep(void)
 {
   set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
   sleep_enable();
-  attachInterrupt(0, wakeUp, LOW);
+  attachInterrupt(0, WakeUP, LOW);
   WDTCSR |= _BV(WDIE);
   sleep_mode();                        // System sleeps here
   // The WDT_vect interrupt wakes the MCU from here
